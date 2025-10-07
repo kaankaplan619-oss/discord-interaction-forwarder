58f6a1535b7105bfd09d5f112bfa40bfdf65cd8f
kaankaplan619-oss <kaankaplan619@gmail.com>
2025-10-08 01:22:23 +0200

    Update index.js
    Ajout GET pour test navigateur

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 export default function handler(req, res) {
+  if (req.method === 'GET') {
+    return res.status(200).send("API Discord est en ligne ✅");
+  }
+
   if (req.method === 'POST') {
     const { type } = req.body;
 
-    // Type 1 = ping
     if (type === 1) {
       return res.status(200).json({ type: 1 });
     }
 
-    // Réponse à une interaction
     return res.status(200).json({
       type: 4,
       data: {
